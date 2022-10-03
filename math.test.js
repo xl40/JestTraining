d43f073767e6fd430a0778f438e1d629eca83915
xuelin <353006063@qq.com>
2022-10-03 10:41:45 +0800

    匹配器 -> .toBe,以及其他一些

--- math.test.js
+++ math.test.js
@@ -1,7 +1,20 @@
-const math = require('./math.js')
-import {add, minus, multi} from './math';
-
-test('aaa',()=>{
-    expect(add(3, 7))
+test('测试加法 3 + 7', () => {
+    expect(10)
         .toBe(10);
+})
+
+test('测试对象内容相等', () => {
+    const a = { one : 1}
+    expect(a).toEqual({ one : 1});
+})
+
+
+test('toBeNull', () => {
+    const a = null
+    expect(a).toBeNull();
+})
+
+test('toBeUndefined', () => {
+    const a = undefined
+    expect(a).toBeUndefined();
 })
